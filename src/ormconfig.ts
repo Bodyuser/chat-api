716f26510a2e8dd7bb4232d1ef62259e49d9ebb5
Git <104937558+Bodyuser@users.noreply.github.com>
2023-06-18 18:37:09 +0300

    commit 6

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,18 +1,21 @@
 import { DataSource, DataSourceOptions } from 'typeorm'
-import * as dotenv from 'dotenv'
+// import * as dotenv from 'dotenv'
 
-dotenv.config()
+// dotenv.config()
 
 export const dataSourceOptions: DataSourceOptions = {
 	port: 5432,
 	type: 'postgres',
-	host: 'snuffleupagus.db.elephantsql.com',
-	username: 'koejsyje',
-	password: 'Dxz6-3WW5AkOy_vfFCSA7ZctPY11sUyq',
-	database: 'koejsyje',
+	name: 'chat-db',
+	url: 'postgres://koejsyje:Dxz6-3WW5AkOy_vfFCSA7ZctPY11sUyq@snuffleupagus.db.elephantsql.com/koejsyje',
+	logging: true,
+	// host: 'snuffleupagus.db.elephantsql.com',
+	// username: 'koejsyje',
+	// password: 'Dxz6-3WW5AkOy_vfFCSA7ZctPY11sUyq',
+	// database: 'koejsyje',
 	synchronize: false,
 	entities: ['dist/**/*.entity.js'],
-	migrations: ['dist/migrations/*.js'],
+	// migrations: ['dist/migrations/*.js'],
 }
 
 const dataSource = new DataSource(dataSourceOptions)
