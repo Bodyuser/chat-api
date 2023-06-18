38e69e946e23b7ee077175a4d76162741a4b4a1a
Git <104937558+Bodyuser@users.noreply.github.com>
2023-06-18 18:54:35 +0300

    commit 7

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -6,15 +6,16 @@ import { DataSource, DataSourceOptions } from 'typeorm'
 export const dataSourceOptions: DataSourceOptions = {
 	port: 5432,
 	type: 'postgres',
-	name: 'chat-db',
 	url: 'postgres://koejsyje:Dxz6-3WW5AkOy_vfFCSA7ZctPY11sUyq@snuffleupagus.db.elephantsql.com/koejsyje',
 	logging: true,
+	synchronize: false,
+	entities: ['dist/**/*.entity.js'],
+
 	// host: 'snuffleupagus.db.elephantsql.com',
 	// username: 'koejsyje',
 	// password: 'Dxz6-3WW5AkOy_vfFCSA7ZctPY11sUyq',
 	// database: 'koejsyje',
-	synchronize: false,
-	entities: ['dist/**/*.entity.js'],
+
 	// migrations: ['dist/migrations/*.js'],
 }
 
